ab678855b5243451480aa642b28fcefa708872af
zhangbofeng <bofeng.zhang@zhaopin.com.cn>
2025-07-06 12:33:54 +0800

    fix HoppedDecryptByText

--- src/main/java/com/chac/util/HoppedDecryptByText.java
+++ src/main/java/com/chac/util/HoppedDecryptByText.java
@@ -6,14 +6,10 @@ package com.chac.util;//
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.Security;
-import java.util.Base64;
 
 public class HoppedDecryptByText {
 
@@ -22,8 +18,8 @@ public class HoppedDecryptByText {
 
 
     public static void main(String[] args) throws Exception {
-        String filePath = "/Users/zhangbofeng/git_hub_projects/springboot-chac-code/src/main/resources/decryptText.txt"; // 替换为你的文件路径
-        //读取文件内容
+        String filePath = "******/xxx.txt"; // 替换为你的文件路径
+        //字符串过长时，字段不允许声明，可以借助外部txt文件 读取txt文件内容
         String s2 = new String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8);
         System.out.println(s2);
         String s1 = HoppedEncryptUtil.decrypt(s2, HoppedEncryptUtil.APP_SECRET, HoppedEncryptUtil.AES_IV);
